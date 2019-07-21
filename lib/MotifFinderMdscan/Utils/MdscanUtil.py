7eb7146c36196022f7aa305431dbb4be8a036616
manish <mandecent.gupta@gmail.com>
2019-07-21 02:29:46 -0400

    output parser added

--- lib/MotifFinderMdscan/Utils/MdscanUtil.py
+++ lib/MotifFinderMdscan/Utils/MdscanUtil.py
@@ -7,7 +7,7 @@ from Bio import motifs
 from Bio import SeqIO
 from Bio.Alphabet import IUPAC
 from io import StringIO
-from installed_clients.DataFileUtilClient import DataFileUtil
+#from installed_clients.DataFileUtilClient import DataFileUtil
 import shutil
 import subprocess
 
@@ -43,7 +43,7 @@ class MdscanUtil:
       alphabet=['A','C','G','T']
       print(path)
   
-      motifSet=[]
+      
       motifList['Condition']='temp'
       motifList['SequenceSet_ref']='123'
 
@@ -54,8 +54,7 @@ class MdscanUtil:
       background['T']=0.0
 
       
-      motifDict['PWM'] = []
-      motifDict['PFM'] = []
+      
 
       
       pwmList=[]
@@ -63,23 +62,33 @@ class MdscanUtil:
       pfmDict={}
       rowList = []
       rowDict={}
-      '''
+      sequence=''
+      idflag=0
       for filename in os.listdir(path):
           outputFileList.append(path + '/' + filename)
           if(filename=="SeqSet.out"):
              outputFilePath=path+'/'+filename
-             print(outputFilePath)
+             #print(outputFilePath)
              samplerFile = open(outputFilePath,'r')
+             motifSet=[]
              for line in samplerFile:
                  line=line.rstrip()
-                 if(line.startswith("#id:")):
-                   #print(locList)
-                   motifSet.append(motifDict)
+                 
+                 if(line.startswith("Motif")):
+                   
+                   if(len(motifDict)!=0):
+                      #print("+++")
+                      motifSet.append(motifDict)
+                      #print(motifDict)
+                      motifDict={}
+                      #print("+++")
                    motifDict['Motif_Locations'] = []
+                   motifDict['PWM'] = []
+                   motifDict['PFM'] = []
                    locList=[]
                    seqflag=True
-                   seq=line.split("\t")
-                   consensus=(seq[1]).split(" ")[1]
+                   seq=line.split(";")
+                   consensus=(seq[3]).split(" ")[2]
                    pwmDict['A']=[]
                    pwmDict['C']=[]
                    pwmDict['G']=[]
@@ -93,36 +102,56 @@ class MdscanUtil:
                    motifDict['PFM']=pfmDict
                    motifDict['Iupac_sequence']=consensus
                    #print(consensus)
+                   locDict={}
                  if(seqflag):
-                     if(line.endswith(";")):
-                        out=line.split(" ")
-                        rec=(out[0]).split("\t")
-                        seqid=rec[0]
-                        orientation=rec[6]
-                        if(orientation == "+"):
-                           seq_start=rec[3]
-                           seq_end=rec[4]
+                     
+                     if(line.startswith(">")):
+                        out=line.split("\t")
+                        idflag=True
+                        #print(out)
+                        rec=(out[3]).split(" ")
+
+                        seqid=(out[0]).replace(">", "")
+                        #print(seqid)
+                        orientation=rec[0]
+                        if(orientation == "f"):
+                           seq_start=int(rec[1])
+                           seq_end=int(rec[1])+8
+                           orientation='+'
                         else:
-                           seq_start=rec[4]
-                           seq_end=rec[3]
-                        sequence=(out[3]).replace(";", "").replace("\"", "")
-                        locDict={}
+                           seq_start=int(rec[1])-8
+                           seq_end=int(rec[1])
+                           orientation='-'
+
+                        '''sequence=(out[3]).replace(";", "").replace("\"", "")'''
+                        #locDict={}
                         locDict['sequence_id']=seqid;
                         locDict['start']=seq_start;
                         locDict['end']=seq_end;
-                        locDict['sequence']=sequence;
+                        #locDict['sequence']=sequence;
                         locDict['orientation']=orientation;
-                        motifDict['Motif_Locations'].append(locDict)
-                        #print(seqid+"\t"+sequence+"\t"+strand+"\t"+start+"\t"+stop)
-                   
-                        locList.append(line)
+                        #motifDict['Motif_Locations'].append(locDict)
+                     else:
+                        if(idflag):
+                           sequence=line
+                           #print(line)
+                           locDict['sequence']=sequence;
+                           #print(locDict)
+                           motifDict['Motif_Locations'].append(locDict)
+                           locDict={}
+                           idflag=False
              else:
+                 #print("+++")
                  motifSet.append(motifDict)
-
+                 #print(motifDict)
+                 motifDict={}
+                 #print("+++")
+                 
+             #print(motifSet)       
+    
       motifList['Motifs']=motifSet
       motifList['Background']=background
-      motifList['Alphabet']=alphabet  
-      #print(motifList) '''    
+      motifList['Alphabet']=alphabet   
       return motifList
 
   def UploadFromMdscan(self, callback_url, params):
@@ -164,7 +193,13 @@ class MdscanUtil:
           if not isinstance(output, dict):
               raise ValueError('Method UploadFrommfmd return value ' +
                              'output is not type dict as required.')
+
+
           # return the results
           return [output]
+
+MDU=MdscanUtil()
+output=MDU.parse_mdscan_output("/home/manish/Desktop/reorganization/reorg/MotifFinderMdscan/test_local/workdir/tmp/mdscan_out")
+print(output)
 
 
